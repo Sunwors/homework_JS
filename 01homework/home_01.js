fae91580dff2b8e8b68cc21a5c82d6ff7dacf964
Sunwors <sunwors@gmail.com>
2024-11-06 18:44:51 +0200

    add homework

---
+++ 01homework/home_01.js
@@ -0,0 +1,46 @@
+
+let a = "hello";
+let b = "awu";
+let c = "com";
+let d =  "ua";
+let e = 1;
+let f = 10;
+let g = - 999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+console.log(k)
+console.log(l)
+console.log(m)
+
+
+let z = 100;
+let q = "100";
+let x = true;
+console.log(typeof z);
+console.log(typeof q);
+console.log(typeof x);
+
+
+let firstName = "Welgus";
+let middleName = "Oleg";
+let lastName = "Romanovych";
+let user = firstName+" "+middleName+" "+lastName;
+console.log(user);
+
+
+
---
+++ 02homework/home_02.js
@@ -0,0 +1,72 @@
+let telefon = [
+    "iPhone X",
+    "iPhone11",
+    "iPhone12",
+    "iPhone 13",
+    "iPhone14",
+    "iPhone15",
+    "iPhone6",
+    "iPhone7",
+    "iPhone8",
+    "iPhone5s",
+];
+console.log(telefon);
+
+let bookData = {
+    title: "encyclopedia",
+    pages: 1000,
+    genre: "ducational",
+    author: [
+        {
+            name: "Petro",
+            age: 40,
+        },
+        {
+            name: "Ivan",
+            age: 50,
+        },
+    ],
+    title: "encyclopedia",
+    pages: 1000,
+    genre: "ducational",
+    author: [
+        {
+            name: "Petro",
+            age: 40,
+        },
+        {
+            name: "Ivan",
+            age: 50,
+        },
+    ],
+
+};
+console.log(bookData);
+
+const users = [
+    {name: "Alice", username: "alice01", password: "passAlice"},
+    {name: "Bob", username: "bob02", password: "passBob"},
+    {name: "Charlie", username: "charlie03", password: "passCharlie"},
+    {name: "David", username: "david04", password: "passDavid"},
+    {name: "Eve", username: "eve05", password: "passEve"},
+    {name: "Frank", username: "frank06", password: "passFrank"},
+    {name: "Grace", username: "grace07", password: "passGrace"},
+    {name: "Hank", username: "hank08", password: "passHank"},
+    {name: "Ivy", username: "ivy09", password: "passIvy"},
+    {name: "Jack", username: "jack10", password: "passJack"},
+];
+
+console.log(users[0].name)
+console.log(users)
+
+let temperatures = [
+    [15, 22, 18],
+    [16, 23, 19],
+    [14, 21, 17],
+    [15, 22, 18],
+    [17, 24, 20],
+    [18, 25, 21],
+    [16, 23, 19]
+];
+
+console.log(temperatures[0], [3, [4]])
